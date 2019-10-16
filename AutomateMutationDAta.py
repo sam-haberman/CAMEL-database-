ba55e6e6c2249024a561e12c52166502d27f41d6
Sam H <samuelhaberman@gmail.com>
2019-10-16 17:20:15 +0200

    Create AutomateMutationDAta.py
    creating script for mutation data conversion

---
+++ AutomateMutationDAta.py
@@ -0,0 +1,7 @@
+# Script that will take in a mutation csv from AleDb and convert it to the Camel Mutation format
+# Starting by just doing one file
+# Then move to go through all files in a folder
+
+
+
+
