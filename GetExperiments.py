d0cdf40c5bcf08bc8d2d94009ff8dcedb3d1796b
Sam H <samuelhaberman@gmail.com>
2019-11-17 16:13:09 +0100

    Update GetExperiments.py
    Messy, but this gets me all the API information for all E. coli and S. cerevisiae experiments on the CAMEL database.
    
    I filtered out to only keep relevant information for tracking information gathering.

--- GetExperiments.py
+++ GetExperiments.py
@@ -1,6 +1,24 @@
 # Script to take data from API and export to Excel so I can get relevant information rather than hand searching through
 # the database
 import json
+import requests
+import pandas as pd
 
-result = json.loads("https://cameldatabase.com/CAMEL/api/experiment?1=Saccharomyces%20cerevisiae")
-print(result['name'])
+experiments = requests.get("https://cameldatabase.com/CAMEL/api/experiment?1=Saccharomyces%20cerevisiae").json()
+eco = requests.get("https://cameldatabase.com/CAMEL/api/experiment?1=Escherichia%20coli").json()
+
+df1 = pd.DataFrame.from_dict(experiments)
+df2 = pd.DataFrame.from_dict(eco)
+
+frames = [df1, df2]
+df = pd.concat(frames)
+df11 = pd.DataFrame(df['name'])
+df22 = pd.DataFrame(df['fields'].values.tolist())
+df33 = pd.DataFrame(df['id'])
+df44 = pd.DataFrame(df['references'].values.tolist())
+df55 = pd.DataFrame(df44[0].values.tolist())
+df_merge = df11.reset_index().merge(df22.reset_index(), left_index=True, right_index=True, how='left')
+df_merge1 = df_merge.reset_index().merge(df33.reset_index(), left_index=True, right_index=True, how='left')
+df_merge2 = df_merge1.reset_index().merge(df55.reset_index(), left_index=True, right_index=True, how='left')
+print(df_merge2)
+df_merge2.to_excel("Experiment_information" + '.xlsx', index=False)
