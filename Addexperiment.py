739f884476e2bf7ff7893488a3f454a76cb220f6
Sam H <samuelhaberman@gmail.com>
2020-02-21 15:10:54 +0100

    Update Addexperiment.py
    added function to add mutation data to experiment

--- Addexperiment.py
+++ Addexperiment.py
@@ -3,7 +3,7 @@ import pandas as pd
 import glob
 path = ""
 # Create function to pull all data from excel file, log in and add experiment with or without a given experiment id
-
+# Also includes function to attach mutation data to an experiment
 
 def get_data_and_add_experiment(file,eid=""):
 
@@ -34,7 +34,7 @@ def get_data_and_add_experiment(file,eid=""):
             fielddict[str(start)] = {'new_' + str(counter): val[start]}
             counter += 1
         start += 1
-    print(fielddict)
+    # print(fielddict)
 
     #Get Reference information
     pubmed_id = val[-2]
@@ -107,7 +107,7 @@ def get_data_and_add_experiment(file,eid=""):
     # The JSON answer will be the same experiment, but with an assigned ID
     if eid == "":
         answer = req.post(exp_url, headers=auth_header, json=new_experiment).json()
-        print(answer)
+        # print(answer)
         exp_id = answer['id']
         added_exp__url = exp_url + "/" + str(exp_id)
     # else:
@@ -117,6 +117,67 @@ def get_data_and_add_experiment(file,eid=""):
 
     # Field value id's will not be assigned yet, until we request the complete object again
     added_experiment = req.get(added_exp__url).json()
+
+# Function to add mutation data to experiments, needs to be .xlsx
+
+def add_mutation_to_experiment(mutation_file,eid):
+    ## URLS
+    base_url = "https://cameldatabase.com/CAMEL/"
+    api_url = base_url + "api"
+    auth_url = base_url + "auth"
+
+    exp_url = api_url + "/experiment"
+    attach_url = api_url + '/attachment'
+
+    ## Credentials
+    login = ''
+    password = ''
+
+    if not password:
+        import getpass
+        password = getpass.getpass()
+
+    ## Get an authentication token
+    '''
+    All editting operations require a header containing a valid AuthToken.
+    A token stays valid for one day.
+    '''
+    auth_request = req.get(auth_url, auth=(login, password))
+    token = auth_request.headers['AuthToken']
+
+    ## Create the header we're going to send along
+    auth_header = {'AuthToken': token}
+
+    local_file_name = mutation_file
+    exp_id = eid
+    added_exp_url = exp_url + '/' + str(exp_id)
+
+    ##We upload the file to a temporary location on the server
+    attachment = {'file': open(local_file_name, 'rb')}
+    resp = req.post(attach_url, files=attachment, headers=auth_header)
+
+    ##Get the temporary id of the upload
+    tmp_uuid = resp.json()['uuid']
+
+    ##Set the attachment field to the tmp id and name the file
+    ##The file will be moved to the correct location
+    dest_file_name = "Mutation Data"
+    attach_exp = {
+        'fields': {
+            '36': {
+                'new_1': {
+                    'uuid': tmp_uuid,
+                    'filename': dest_file_name}
+            }
+        }
+    }
+    resp = req.put(added_exp_url, headers=auth_header, json=attach_exp)
+    if resp.ok:
+        print("Upload successful")
+    else:
+        print("Upload failed")
+
+
 
 def remove_experiment(eid):
 
@@ -159,3 +220,6 @@ def remove_experiment(eid):
 # Adding experiments from a folder rather than individually
 #  for fname in glob.glob(path):
 #     get_data_and_add_experiment(fname,)
+
+#add_mutation_to_experiment('C:/Users/samue/Desktop/Thesis/Mutationlisttemplate_new.xlsx',757)
+
