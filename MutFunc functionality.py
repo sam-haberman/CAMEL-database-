1d7c5c806a663be96f60a09f2f0d64e655ef1d24
Sam H <samuelhaberman@gmail.com>
2020-03-05 16:41:37 +0100

    MutFunc(E.coli)
    Takes mutation file, retrieves unique SNPs and uploads them to MutFunc returning the results page.
    
    Currently only for E. coli and only for the most common strain

--- MutFunc functionality.py
+++ MutFunc functionality.py
@@ -2,6 +2,10 @@
 # MutFunc(http://www.mutfunc.com/ to provide extra information to the data providers about their mutations
 
 import pandas as pd
+import re
+import mechanize
+
+
 
 # requires an excel file of mutations that should be obtained from the Camel database experiment page. Since MutFunc
 # only works on SNPs the first thing we have to do is remove all non SNPs and only keep unique values
@@ -16,9 +20,24 @@ def runmutfunc(file):
     # Take only data from the SNPs that we want for checking in MutFunc
     # Merge all parts together so they can be added as one entry per line
     SNP_list = pd.DataFrame()
-    SNP_list["SNPs"] = df["CHROM"] + " " + df["START POS"].astype(str) + " " + df["REF"] + " " + df["ALT"]
-    SNP_list.to_csv(file + ".csv")
- # time now to learn how to acess the website through python, looking at the mechanize package
+    SNP_list["SNPs"] = "chr" + " " + df["START POS"].astype(str) + " " + df["REF"] + " " + df["ALT"]
+    # time now to learn how to access the website through python, looking at the mechanize package
+    br = mechanize.Browser()
+    br.open("http://www.mutfunc.com/submit#")
+    br.select_form(nr=0)
+    species = br.form.find_control("tax_id")
+    for item in species.items:
+        if item.name == "2":
+            item.selected = True
+    mutations = ""
+    for i in range(len(SNP_list)):
+        mutations = mutations + SNP_list["SNPs"].iloc[i] + "\n"
+    br.form['muts_text'] = mutations
+    br.submit()
+    base_url = br.geturl()
+    print(base_url)
+
+
 
 
 runmutfunc("C:/Users/samue/Desktop/Thesis/ALEDB_conversion/Experiment_Data/42C.csv.xlsx")
